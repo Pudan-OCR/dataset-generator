4def6f310568f831f6b39426243396ce500090b9
dethaa <sharonmarbun12@gmail.com>
2023-03-16 13:23:07 +0700

    feat(ktp): add noise skew, salt & pepper, blur

--- ktp/mainktp.py
+++ ktp/mainktp.py
@@ -1,6 +1,7 @@
-from PIL import Image, ImageFont, ImageDraw
+from PIL import Image, ImageFont, ImageDraw, ImageFilter
 import random
 from faker import Faker
+import argparse
 
 PROVINSI = [
     "NANGGROE ACEH DARUSSALAM",
@@ -110,68 +111,119 @@ PEKERJAAN = [
 ]
 
 class KTPGenerator:
-    def __init__(self, img_path) -> None:
+    def __init__(self, img_path, args) -> None:
         self.fake = Faker('id_ID')
         self.path = img_path
+        self.args = args
         self.base = Image.open(self.path).convert("RGBA")
-        self.txt_layer = None                      
+        self.txt_layer = None
+        self.SKEW = 3
+        self.NUM = 10
+        self.BLUR = 2
+        self.SALTPEPPER = 10000
+
+    def skew(self):
+        agl = args.skew if args.skew != None else self.SKEW
+        if agl == 0:
+            return
+        self.out = self.out.rotate(random.uniform(-agl, agl), expand=True)
+    
+    def blur(self):
+        rad = args.blur if args.blur != None else self.BLUR
+        if rad == 0:
+            return
+        self.out = self.out.filter(ImageFilter.GaussianBlur(random.uniform(0, rad)))
+    
+    def saltAndPepper(self):
+        max = args.salt_and_pepper if args.salt_and_pepper != None else self.SALTPEPPER
+        row , col = self.out.size
+        num_of_pix = random.randint(300 , max)
+        for _ in range(num_of_pix):
+            x = random.randint(0, row - 1)
+            y = random.randint(0, col - 1)
+            n = random.randint(64, 128)
+            self.out.putpixel((x,y), (n, n, n, 255))
 
     def save(self, i):
-        self.out = Image.alpha_composite(self.base, self.txt_layer)
-        # self.out.show()
         self.out.save(f'out/{i}.png')
+    
+    def create(self):
+        self.txt_layer = Image.new("RGBA", self.base.size, (255, 255, 255, 0))
+        draw = ImageDraw.Draw(self.txt_layer)
 
-    def generate(self, n:int):
+        font_judul = ImageFont.truetype("./font/arlrdbd.ttf", 90)
+        font_nik = ImageFont.truetype("./font/OCR A Extended.ttf", 128)
+        font_data = ImageFont.truetype("./font/arial-bold.ttf", 65)  
+
+        provinsi    = "PROVINSI " + random.choice(PROVINSI)
+        kabupaten   = random.choice(["KABUPATEN ", "KOTA "]) + self.fake.city().upper()
+        nik         = f'{random.randint(0,9999999999999999):016}'
+        nama        = self.fake.first_name().upper() + " " + self.fake.last_name().upper()
+        ttl         = self.fake.city().upper() + ", " + self.fake.date(pattern="%d-%m-%Y")
+        gender      = random.choice(GENDER)
+        golda       = random.choice(GOLDA)
+        alamat      = self.fake.street_address().upper()
+        rt_rw       = f'{random.randint(0,999):03}' + '/' + f'{random.randint(0,999):03}'
+        kelurahan   = self.fake.city().upper()
+        kecamatan   = self.fake.city().upper()
+        agama       = random.choice(AGAMA)
+        perkawinan  = random.choice(PERKAWINAN)
+        pekerjaan   = random.choice(PEKERJAAN)
+        kewarganegaraan = random.choice(["WNI", "WNA"])
+        berlaku     = random.choice(["SEUMUR HIDUP", self.fake.date(pattern="%d-%m-%Y")])
+        tempat      = self.fake.city().upper()
+        tgl         = self.fake.date(pattern="%d-%m-%Y")
+        
+        draw.text((1463, 55), provinsi, font=font_judul, fill=(12,42,59,255), anchor='mt')
+        draw.text((1463, 149), kabupaten, font=font_judul, fill=(12,42,59,255), anchor='mt')
+        draw.text((703, 280), nik, font=font_nik, fill=(12,42,59,255), anchor='lt')
+        draw.text((775, 453), nama, font=font_data, fill=(12,42,59,255), anchor='lt')
+        draw.text((775, 535), ttl, font=font_data, fill=(12,42,59,255), anchor='lt')
+        draw.text((775, 613), gender, font=font_data, fill=(12,42,59,255), anchor='lt')
+        draw.text((1852, 641), golda, font=font_data, fill=(12,42,59,255), anchor='lm')
+        draw.text((775, 699), alamat, font=font_data, fill=(12,42,59,255), anchor='lt')
+        draw.text((775, 781), rt_rw, font=font_data, fill=(12,42,59,255), anchor='lt')
+        draw.text((775, 861), kelurahan, font=font_data, fill=(12,42,59,255), anchor='lt')
+        draw.text((775, 943), kecamatan, font=font_data, fill=(12,42,59,255), anchor='lt')
+        draw.text((775, 1023), agama, font=font_data, fill=(12,42,59,255), anchor='lt')
+        draw.text((775, 1109), perkawinan, font=font_data, fill=(12,42,59,255), anchor='lt')
+        draw.text((775, 1187), pekerjaan, font=font_data, fill=(12,42,59,255), anchor='lt')
+        draw.text((775, 1269), kewarganegaraan, font=font_data, fill=(12,42,59,255), anchor='lt')
+        draw.text((775, 1351), berlaku, font=font_data, fill=(12,42,59,255), anchor='lt')
+        draw.text((2387, 1227), tempat, font=font_data, fill=(12,42,59,255), anchor='mt')
+        draw.text((2387, 1313), tgl, font=font_data, fill=(12,42,59,255), anchor='mt')
+
+        self.out = Image.alpha_composite(self.base, self.txt_layer)
+
+
+    def generate(self):
+        n = args.number if args.number != None else self.NUM
         for i in range(n):
-            self.txt_layer = Image.new("RGBA", self.base.size, (255, 255, 255, 0))
-            draw = ImageDraw.Draw(self.txt_layer)
-
-            font_judul = ImageFont.truetype("arlrdbd.ttf", 90)
-            font_nik = ImageFont.truetype("OCR A Extended.ttf", 128)
-            font_data = ImageFont.truetype("arial-bold.ttf", 65)  
-
-            provinsi    = "PROVINSI " + random.choice(PROVINSI)
-            kabupaten   = random.choice(["KABUPATEN ", "KOTA "]) + self.fake.city().upper()
-            nik         = f'{random.randint(0,9999999999999999):016}'
-            nama        = self.fake.first_name().upper() + " " + self.fake.last_name().upper()
-            ttl         = self.fake.city().upper() + ", " + self.fake.date(pattern="%d-%m-%Y")
-            gender      = random.choice(GENDER)
-            golda       = random.choice(GOLDA)
-            alamat      = self.fake.street_address().upper()
-            rt_rw       = f'{random.randint(0,999):03}' + '/' + f'{random.randint(0,999):03}'
-            kelurahan   = self.fake.city().upper()
-            kecamatan   = self.fake.city().upper()
-            agama       = random.choice(AGAMA)
-            perkawinan  = random.choice(PERKAWINAN)
-            pekerjaan   = random.choice(PEKERJAAN)
-            kewarganegaraan = random.choice(["WNI", "WNA"])
-            berlaku     = random.choice(["SEUMUR HIDUP", self.fake.date(pattern="%d-%m-%Y")])
-            tempat      = self.fake.city().upper()
-            tgl         = self.fake.date(pattern="%d-%m-%Y")
-            
-            draw.text((1463, 55), provinsi, font=font_judul, fill=(0, 0, 0, 255), anchor='mt')
-            draw.text((1463, 149), kabupaten, font=font_judul, fill=(0, 0, 0, 255), anchor='mt')
-            draw.text((703, 280), nik, font=font_nik, fill=(0, 0, 0, 255), anchor='lt')
-            draw.text((775, 453), nama, font=font_data, fill=(0, 0, 0, 255), anchor='lt')
-            draw.text((775, 535), ttl, font=font_data, fill=(0, 0, 0, 255), anchor='lt')
-            draw.text((775, 613), gender, font=font_data, fill=(0, 0, 0, 255), anchor='lt')
-            draw.text((1852, 641), golda, font=font_data, fill=(0, 0, 0, 255), anchor='lm')
-            draw.text((775, 699), alamat, font=font_data, fill=(0, 0, 0, 255), anchor='lt')
-            draw.text((775, 781), rt_rw, font=font_data, fill=(0, 0, 0, 255), anchor='lt')
-            draw.text((775, 861), kelurahan, font=font_data, fill=(0, 0, 0, 255), anchor='lt')
-            draw.text((775, 943), kecamatan, font=font_data, fill=(0, 0, 0, 255), anchor='lt')
-            draw.text((775, 1023), agama, font=font_data, fill=(0, 0, 0, 255), anchor='lt')
-            draw.text((775, 1109), perkawinan, font=font_data, fill=(0, 0, 0, 255), anchor='lt')
-            draw.text((775, 1187), pekerjaan, font=font_data, fill=(0, 0, 0, 255), anchor='lt')
-            draw.text((775, 1269), kewarganegaraan, font=font_data, fill=(0, 0, 0, 255), anchor='lt')
-            draw.text((775, 1351), berlaku, font=font_data, fill=(0, 0, 0, 255), anchor='lt')
-            draw.text((2387, 1227), tempat, font=font_data, fill=(0, 0, 0, 255), anchor='mt')
-            draw.text((2387, 1313), tgl, font=font_data, fill=(0, 0, 0, 255), anchor='mt')
-
+            self.create()
+            self.saltAndPepper()
+            self.blur()
+            self.skew()
             self.save(i)
 
+    def test(self):
+        self.create()
+        self.saltAndPepper()
+        self.blur()
+        self.skew()
+        self.out.show()
 
-Generator = KTPGenerator("test.jpg")
-Generator.generate(10)
+if __name__ == "__main__":
+    argParser = argparse.ArgumentParser()
+    argParser.add_argument("-t", "--test", type=bool, help="True for test")
+    argParser.add_argument("-n", "--number", type=float, help="Number of generated dataset (default = 10)")
+    argParser.add_argument("-s", "--skew", type=int, help="Max skew angle in degree (default= 3, 0 to switch off")
+    argParser.add_argument("-b", "--blur", type=float, help="Max gaussian blur radius (default = 1.2, 0 to switch off")
+    argParser.add_argument("-sp", "--salt_and_pepper", type=int, help="Salt and pepper density (default = 5000)")
+    args = argParser.parse_args()
 
+    Generator = KTPGenerator("test.jpg", args=args)
 
+    if args.test == True:
+        Generator.test()
+    else:
+        Generator.generate()
