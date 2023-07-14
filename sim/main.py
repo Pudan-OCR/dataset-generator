71bde1ef9caaf4f4344f5311bede1e6dfcf835db
13519016 <13519016@std.stei.itb.ac.id>
2023-07-14 14:28:36 +0700

    menambahkan bounding box untuk sim

--- sim/main.py
+++ sim/main.py
@@ -2,6 +2,7 @@ from PIL import Image, ImageFont, ImageDraw, ImageFilter
 import random
 from faker import Faker
 import argparse
+import math
 
 TYPES = ["A", "BI", "BII", "C", "D"]
 GOLDA = ["A", "B", "AB", "O"]
@@ -96,6 +97,19 @@ POLDAS = [
 
 # SIM: 85.60 mm x 53.98 mm  (sama juga untuk KTP)
 # Resize image -> 720 x 453 px (asumsi pinggiran SIM tepat pada tepi image)  
+def rotate_dot(x, y, angle):
+    # Convert angle to radians
+    angle_rad = math.radians(angle)
+
+    # Calculate the sine and cosine of the angle
+    cos_angle = math.cos(angle_rad)
+    sin_angle = math.sin(angle_rad)
+
+    # Perform the rotation transformation
+    new_x = x * cos_angle - y * sin_angle
+    new_y = x * sin_angle + y * cos_angle
+
+    return new_x, new_y
 
 class SIMGenerator:
     SKEW = 3
@@ -112,50 +126,110 @@ class SIMGenerator:
         self.txt_layer = None                   # Image.new("RGBA", self.base.size, (255, 255, 255, 0))
         self.draw = None                        # ImageDraw.Draw(self.txt_layer)
         self.args = None
+        self.boundingbox = []
+        self.predicted = []
 
     def typeDrawer(self, type):
         font = ImageFont.truetype("arial.ttf", 56)
         self.draw.text((550, 85), type, font=font, fill=(0, 0, 0, 220), anchor='lt',  stroke_width=2)
+        left,top,right,bottom = self.draw.textbbox((550, 85),type, font= font, anchor='lt',stroke_width=2)
+        bb = [(left,top),(right,top),(right,bottom),(left,bottom)]
+        self.boundingbox.append(bb)
+        self.predicted.append(type)
 
     def namaDrawer(self, name):
         font = ImageFont.truetype("./font/arial-bold.ttf", 23)
         self.draw.text((250, 177), name, font=font, fill=(0, 0, 0, 220), anchor='lt')
+        left,top,right,bottom = self.draw.textbbox((250, 177),name, font= font, anchor='lt')
+        bb = [(220, 175),(right,top),(right,bottom),(219, 193)]
+        self.boundingbox.append(bb)
+        self.predicted.append(name)
 
     def ttlDrawer(self, ttl):
         font = ImageFont.truetype("./font/arial-bold.ttf", 23)
         self.draw.text((250, 205), ttl, font=font, fill=(0, 0, 0, 220), anchor='lt')
+        left,top,right,bottom = self.draw.textbbox((250, 205),ttl, font= font, anchor='lt')
+        bb = [(220, 202),(right,top),(right,bottom),(219, 223)]
+        self.boundingbox.append(bb)
+        self.predicted.append(ttl)
 
     def golGenderDrawer(self, golgen):
         font = ImageFont.truetype("./font/arial-bold.ttf", 23)
         self.draw.text((250, 233), golgen, font=font, fill=(0, 0, 0, 220), anchor='lt')
+        left,top,right,bottom = self.draw.textbbox((250, 233),golgen, font= font, anchor='lt')
+        bb = [(217, 229),(right,top),(right,bottom),(216, 251)]
+        self.boundingbox.append(bb)
+        self.predicted.append(golgen)
 
     def almtDrawer(self, line1, line2, line3):
         font = ImageFont.truetype("./font/arial-bold.ttf", 23)
         self.draw.text((250, 261), line1, font=font, fill=(0, 0, 0, 220), anchor='lt')
+        left,top,right,bottom = self.draw.textbbox((250, 261),line1, font= font, anchor='lt')
+        bb = [(216, 260),(right,top),(right,bottom),(215, 279)]
+        self.boundingbox.append(bb)
+        self.predicted.append(line1)
         self.draw.text((250, 289), line2, font=font, fill=(0, 0, 0, 220), anchor='lt')
+        left,top,right,bottom = self.draw.textbbox((250, 289),line2, font= font, anchor='lt')
+        bb = [(left,top),(right,top),(right,bottom),(left,bottom)]
+        self.boundingbox.append(bb)
+        self.predicted.append(line2)
         self.draw.text((250, 317), line3, font=font, fill=(0, 0, 0, 220), anchor='lt')
+        left,top,right,bottom = self.draw.textbbox((250, 317),line3, font= font, anchor='lt')
+        bb = [(left,top),(right,top),(right,bottom),(left,bottom)]
+        self.boundingbox.append(bb)
+        self.predicted.append(line3)
 
     def profDrawer(self, prof):
         font = ImageFont.truetype("./font/arial-bold.ttf", 23)
         self.draw.text((250, 343), prof, font=font, fill=(0, 0, 0, 220), anchor='lt')
+        left,top,right,bottom = self.draw.textbbox((250, 343),prof, font= font, anchor='lt')
+        bb = [(217, 340),(right,top),(right,bottom),(216, 360)]
+        self.boundingbox.append(bb)
+        self.predicted.append(prof)
 
     def polDrawer(self, pol):
         font = ImageFont.truetype("./font/arial-bold.ttf", 23)
         self.draw.text((250, 370), pol, font=font, fill=(0, 0, 0, 220), anchor='lt')
+        left,top,right,bottom = self.draw.textbbox((250, 370),pol, font= font, anchor='lt')
+        bb = [(218, 367),(right,top),(right,bottom),(217, 389)]
+        self.boundingbox.append(bb)
+        self.predicted.append(pol)
 
     def noDrawer(self, no):
         font = ImageFont.truetype("./font/arial-bold.ttf", 25)
         self.draw.text((565, 150), no, font=font, fill=(0, 0, 0, 220), anchor='mm')
+        left,top,right,bottom = self.draw.textbbox((565, 150),no, font= font, anchor='mm')
+        bb = [(left,top),(right,top),(right,bottom),(left,bottom)]
+        self.boundingbox.append(bb)
+        self.predicted.append(no)
 
     def tglBwhDrawer(self, tgl):
         font = ImageFont.truetype("./font/arial-bold.ttf", 23)
         self.draw.text((590, 432), tgl, font=font, fill=(0, 0, 0, 220), anchor='mm')
+        left,top,right,bottom = self.draw.textbbox((590, 432),tgl, font= font, anchor='mm')
+        bb = [(left,top),(right,top),(right,bottom),(left,bottom)]
+        self.boundingbox.append(bb)
+        self.predicted.append(tgl)
 
     def skewNoise(self):
         agl = args.skew if args.skew != None else self.SKEW
         if agl == 0:
             return
-        self.out = self.out.rotate(random.uniform(-agl, agl), expand=True)
+        angle = random.uniform(-agl, agl)
+        width, heigh = self.out.size
+        self.out = self.out.rotate(angle, expand=True)
+        width_after_rotate, heigh_after_rotate = self.out.size
+        width_differ = width_after_rotate - width
+        height_differ = heigh_after_rotate - heigh
+
+        for i in range(len(self.boundingbox)):
+            for j in range(4):
+                dot = self.boundingbox[i][j]
+                x,y = rotate_dot(dot[0],dot[1],angle*(-1))
+                if(angle>0):
+                    self.boundingbox[i][j] = (x,y+height_differ)
+                else:
+                    self.boundingbox[i][j] = (x+width_differ,y)
 
     def blur(self):
         rad = args.blur if args.blur != None else self.BLUR
@@ -174,11 +248,33 @@ class SIMGenerator:
             self.out.putpixel((x,y), (n, n, n, 255))
             
     def save(self, i):
-        self.out.save(f'out/{i}.png')
+        self.out = self.out.convert('RGB')
+        self.out.save(f'examples/image/img_{i}.jpg')
+        f=open(f'examples/label/gt_img_{i}.txt','w+')
+        for i in range(len(self.boundingbox)):
+            txt = ""
+            for point in self.boundingbox[i]:
+                for each in point:
+                    txt += str(int(each)) + ','
+            txt += self.predicted[i]
+            f.write(txt+"\n")
+        f.close()
+
+    def showBoundingBox(self):
+        for poly in self.boundingbox:
+            bb_layer = Image.new("RGBA",self.out.size, (255,255,255,0))
+            temp_draw = ImageDraw.Draw(bb_layer)
+            temp_draw = temp_draw.polygon(poly,outline="blue")
+            self.out = Image.alpha_composite(self.out,bb_layer)
+        self.out.show()
 
     def create(self):
         self.txt_layer = Image.new("RGBA", self.base.size, (255, 255, 255, 0))
         self.draw = ImageDraw.Draw(self.txt_layer)
+        self.boundingbox = [[(190, 31), (437, 33), (436, 69), (190, 67)],
+                            [(487, 52), (670, 57), (670, 78), (487, 74)],
+                            [(192, 72), (443, 75), (443, 95), (192, 92)]]
+        self.predicted = ["INDONESIA","DRIVING LICENSE","SURAT IZIN MENGEMUDI"]
 
         type    = random.choice(TYPES)
         name    = self.fake.first_name().upper() + " " + self.fake.last_name().upper()
@@ -218,13 +314,13 @@ class SIMGenerator:
         self.saltAndPepper()
         self.blur()
         self.skewNoise()
-        self.out.show()
+        self.showBoundingBox()
 
 if __name__ == "__main__":
     # MAIN PROGRAM
     argParser = argparse.ArgumentParser()
     argParser.add_argument("-t", "--test", type=bool, help="True for test")
-    argParser.add_argument("-n", "--number", type=float, help="Number of generated dataset (default = 10)")
+    argParser.add_argument("-n", "--number", type=int, help="Number of generated dataset (default = 10)")
     argParser.add_argument("-s", "--skew", type=int, help="Max skew angle in degree (default= 3, 0 to switch off")
     argParser.add_argument("-b", "--blur", type=float, help="Max gaussian blur radius (default = 1.2, 0 to switch off")
     argParser.add_argument("-sp", "--salt_and_pepper", type=int, help="Salt and pepper density (default = 5000)")
